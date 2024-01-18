e5c9184b22773380c79dbcc71f1c581e24b518fa
nightwatchman17 <munmahmud99@gmail.com>
2024-01-18 18:16:05 +0600

    first commit

---
+++ a.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+void Solve()
+{
+     cout << "TEST\n";
+}
+
+int main()
+{
+     ios_base::sync_with_stdio(false);
+     cin.tie(NULL);
+     int tests = 1;
+     // cin >> tests;
+     while (tests--)
+     {
+          Solve();
+     }
+     return 0;
+}
+
+// in the end, it doesn't even matter
