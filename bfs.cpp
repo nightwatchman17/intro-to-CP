ae4135104f400eae3721512fed342a568efa7b61
nightwatchman17 <munmahmud99@gmail.com>
2024-01-18 18:45:41 +0600

    graph

---
+++ bfs.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+bool vis[100];
+void bfs(int src)
+{
+     vis[src] = 1;
+}
+
+void Solve()
+{
+}
+
+int main()
+{
+     ios_base::sync_with_stdio(false);
+     cin.tie(NULL);
+     int tests = 1;
+     // cin >> tests;
+     while (tests--)
+          Solve();
+     return 0;
+}
+
+// in the end, it doesn't even matter
