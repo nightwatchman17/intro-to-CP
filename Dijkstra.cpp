cb4ac2ac6e63759305ff7aa3e08432dd3d639931
nightwatchman17 <munmahmud99@gmail.com>
2024-01-21 22:42:04 +0600

    abcd

--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -1,33 +1,31 @@
 // Dijkstra Allgorithm (SSSP in weighted graph)
 
-const int inf = 3e5;
+const int inf = 1e5 + 125;
 int node, edge;
 vector<pair<int, int>> adj[inf];
-ll dist[inf];
-
+ll cost[inf]; // shortest distance will be stored here
 void dijkstra(int src)
 {
-     for (int i = 0; i < inf; i++)
-          dist[i] = 2e18;
-     priority_queue<pair<ll, int>, vector<pair<ll, int>>, greater<pair<ll, int>>> pq;
-     pq.push({0LL, src});
-     dist[src] = 0;
+     for (int i = 1; i <= node; i++)
+          cost[i] = 2e18;                                                             // initialization with INFINITY cost
+     cost[src] = 0;                                                                   // distance of source is 0
+     priority_queue<pair<ll, int>, vector<pair<ll, int>>, greater<pair<ll, int>>> pq; // {distance, node}
+     pq.push({0, src});
      while (!pq.empty())
      {
-          pair<ll, int> at = pq.top();
-          ll curDis = at.first;
+          pair<ll, int> at = pq.top(); // current node
           int curNode = at.second;
+          ll curDist = at.first;
           pq.pop();
-          if (dist[curNode] < curDis)
-               continue;
+          if (cost[curNode] < curDist)
+               continue; // do not need relax operation
           for (auto u : adj[curNode])
           {
-               int child = u.first;
-               ll wt = u.second;
-               if (curDis + wt < dist[child])
-               {
-                    dist[child] = curDis + wt;
-                    pq.push({dist[child], child});
+               int child = u.first, wt = u.second;
+               if (curDist + wt < cost[child])
+               {                                   // relax operation
+                    cost[child] = curDist + wt;    // update
+                    pq.push({cost[child], child}); // push into priority queue
                }
           }
      }
