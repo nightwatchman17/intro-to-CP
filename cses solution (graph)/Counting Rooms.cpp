b6ac33577e78673a355e0efa63afaa57700d22b8
nightwatchman17 <munmahmud99@gmail.com>
2024-01-28 02:09:01 +0600

    cses

---
+++ cses solution (graph)/Counting Rooms.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+void Solve()
+{
+}
+
+int main()
+{
+   ios_base::sync_with_stdio(false);
+   cin.tie(NULL);
+   int tests = 1;
+   // cin >> tests;
+   while (tests--)
+      Solve();
+   return 0;
+}
+
+// in the end, it doesn't even matter
